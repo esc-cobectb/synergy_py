b146ae011d47328862cbee5e192962e66be054fe
Ilya Abrosimov <eli.abrosimov@gmail.com>
2023-11-16 18:43:40 +0300

    Исправление 3 задания в домашней работе 6: Функции часть 1. Домашнее задание.

--- 6/3.py
+++ 6/3.py
@@ -1,5 +1,10 @@
 def maxOfNumbers(arg1, arg2, arg3 = 0):
-	return max(arg1, arg2, arg3)
+	if(arg1 >= arg2 and arg1 >= arg3):
+		return arg1
+	elif (arg2 >= arg1 and arg2 >= arg3):
+		return arg2
+	else :
+		return arg3
 
 args = list(map(int, input().split()))
 if(len(args) == 3):
